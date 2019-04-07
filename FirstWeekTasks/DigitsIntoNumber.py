03d97f7de80acfa8446b21d45197f7a7be664c15
VladislavSpassov <39959092+VladislavSpassov@users.noreply.github.com>
2019-04-07 10:53:30 +0300

    Update DigitsIntoNumber.py

--- FirstWeekTasks/DigitsIntoNumber.py
+++ FirstWeekTasks/DigitsIntoNumber.py
@@ -2,19 +2,7 @@ def to_number(digits):
     ar =[]
     s = ""
     for v in digits:
-        if(v >= "0" and v <= "9"):
-            s = s + str(v)
+        s = s + str(v)
     
     return s
     
-    count = 0;
-    #while(count != len(ar)):
-     #   reversedAr[count] = 
-    """while (di >= 0):
-        print(ar)
-        ar.append(di % 10)
-        di //= 10
-    return 0
-"""
-di =input()
-print(to_number(di))
