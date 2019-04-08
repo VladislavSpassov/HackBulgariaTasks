4c269914e3294ddf77acb5cc1cd586b51e2c07b6
VladislavSpassov <39959092+VladislavSpassov@users.noreply.github.com>
2019-04-08 14:27:28 +0300

    Update BusinessCard.py

--- ThirdWeekTasks/BusinessCard.py
+++ ThirdWeekTasks/BusinessCard.py
@@ -1,18 +1,32 @@
 import sys
-from .json2html import *
+import json
+def Convert(filename):
+    f = open(filename,'r')
+    json_data = json.load(f)
+    f.close()
+    new_file = open("newfile.txt","w+")
+    for k,people in json_data.items():
+        for person in people:
+            new_file.write('<!DOCTYPE html>\n<html>\n<head>\n\t<title>'+person['first_name']+' '+person['last_name']+'</title>')
+            new_file.write('\n')
 
-def Convert():
-    j = {"name":"softvar","age":21}
-    formatted_table = json2html.convert(json =j)
-    print(formatted_table)
+            new_file.write('\t<link rel="stylesheet" type="text/css" href="styles.css">')   
+            new_file.write('\n')    
+            new_file.write('</head>')
+            new_file.write('\n')
+            new_file.write('<body>')
+            new_file.write('\n')
+
+            new_file.write('\n\t<div class="business-card' + " " +  person["gender"] + ">")
+            new_file.write("\n\t<h1 class=full-name>" +person['first_name']+' ' + person['last_name']+'</h1>')
+                
 
 
 
+def main():
+    Convert("data1.json")
 
-
-def main(file):
-    Convert()
 if __name__ == '__main__':
-    filename = sys.argv[1]
-    main(filename)
+    #filename = sys.argv[1]
+    main()
 
