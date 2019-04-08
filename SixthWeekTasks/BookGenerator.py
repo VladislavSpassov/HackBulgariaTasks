ca71584e7ec24bc7800a75a31e8be7539e399c94
VladislavSpassov <39959092+VladislavSpassov@users.noreply.github.com>
2019-04-08 13:14:14 +0300

    Update BookGenerator.py

--- SixthWeekTasks/BookGenerator.py
+++ SixthWeekTasks/BookGenerator.py
@@ -1,21 +1,32 @@
 import random 
 
 
-def random_word_generator():
+def random_word_generator(num):
     with open("/etc/dictionaries-common/words","r") as f:
         index = 0
         s = ""
         lines = list(f)
-        result = []
-        while(index < 10):
-            result.append(" ".join((random.sample(lines, 3))))
-            index+=1
+        result = ""
+        words = random.sample(lines,num)
+        print(words)
+        for word in words:
+            result+= word[0:len(word) - 1] + " "
             
-        return "\n".join(result)
+        return result
 
 
-    
+def generateChapter(num_chapters,chapter_length):
+        f = open("book.txt","w+")
+        
+        for index in range(1,num_chapters + 1):
+
+            f.write("# Chapter {0}".format(index))
+            f.write("\n")
+            f.write(random_word_generator(chapter_length))    
+            f.write("\n")
+            f.write("\n")
 
 
 
-print((random_word_generator()))
+
+generateChapter(10,100)
