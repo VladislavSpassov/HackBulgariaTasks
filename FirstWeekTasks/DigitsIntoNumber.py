79258901a2560f6ecc09380f2f7cf3aed69cbfcb
Vladislav Spasov <wladislawspassow@gmail.com>
2019-05-04 17:53:24 +0300

    delete folders

--- FirstWeekTasks/DigitsIntoNumber.py
+++
@@ -1,8 +0,0 @@
-def to_number(digits):
-    ar =[]
-    s = ""
-    for v in digits:
-        s = s + str(v)
-    
-    return s
-    
--- SecondWeekTasks/moneyTrackerTests.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-class MoneyTrackeTests(unittest.TestCase):
-    def Test_Print_Data(self):
-        input =
-        result = "income \n [(700, 'Salary'), (50, 'Savings')] \n expense \n[(4, 'Eating Out')]\n"
-        self.AssertEqual()
-"
