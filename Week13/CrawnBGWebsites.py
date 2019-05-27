66ab9ee5d891b4c2fdd13c6f656a999a860a6c2d
Vladislav Spasov <wladislawspassow@gmail.com>
2019-05-27 18:44:50 +0300

    Adding Data Base

--- Week13/CrawnBGWebsites.py
+++ Week13/CrawnBGWebsites.py
@@ -1,27 +1,50 @@
-import requests 
+import requests
 from bs4 import BeautifulSoup
 from time import sleep
+import json
+from sqlalchemy import create_engine,Column,Integer,String,ForeignKey,table, column, select, update, insert
+from sqlalchemy.ext.declarative import declarative_base
+from urlparse import urlparse
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import *
+
 
 start = "http://register.start.bg/"
 f = requests.get(start)
 soup = BeautifulSoup(f.text, 'html.parser')
-websites = []
+websites = {}
+
+Base = declarative_base()
+engine = create_engine('sqlite:///servers.db', echo = True)
+Session = sessionmaker(bind=engine)
+session = Session()
+metadata = MetaData()
+connection = engine.connect()
+servers = Table('servers', metadata,
+    Column('user_id', Integer, primary_key=True),
+    Column('server', String, nullable=False),
+    Column('website', String)
+)
+count = 0
+metadata.create_all(engine)
 for link in soup.find_all('a'):
     l = link.get('href')
 
     #if(not type(l) is None or not type(l)is unicode):
         #print(l)
+
     ext = ".bg"
     ht = "http"
-    website = {}
-    count = 0 
+
+    print(count)
     if l is None:
         continue
     elif u"link.php" in l :
         try:
             lin = start + str(l) 
-
             obj1 = requests.get(lin)
+            parsed = urlparse(lin)
+
             exists = False
             for key in obj1.headers:
                 if(key == "Server"):
@@ -36,12 +59,11 @@ for link in soup.find_all('a'):
                     #print("different status code from 200")
                     #continue
                 #else:
-                print(l)
-                
-                print(obj1.headers["server"])
-                count+=1
+                if(parsed.netloc not in websites):
+                    engine.execute(servers.insert(),server = obj1.headers["Server"],website =  obj1.url)
+                    count +=1
+
             else:
-                print("Not this time")
                 continue
         except requests.exceptions.ConnectionError as e:
             pass
@@ -58,18 +80,33 @@ for link in soup.find_all('a'):
                 #print("differen t status code from 200")
                 #continue
             #else:
-            print(l)
+            parsed = urlparse(obj.url)
+            
 
-            print(obj.headers["server"])
-            count +=1
+            if(parsed.netloc not in websites):
+                engine.execute(servers.insert(),server = obj.headers["Server"], website = obj.url)
+                count +=1
+            else:
+                websites[parsed.netloc] +=1
         except requests.exceptions.ConnectionError as e:
             pass
 
     else:
-        print("Not gonna work")
         continue
-    
-        #print(obj1)
-    #if(".bg" in l): 
-        #r = requests.get(l)
-        #print(r.headers["Server"])
+    if(count >= 10):
+        break
+
+s = select([distinct(servers.c.server)])
+result = connection.execute(s)
+
+for row in result:
+    websites[row[0]] = 0
+
+
+s = select([servers.c.server])
+result = connection.execute(s)
+
+for row in result:
+    websites[row[0]] +=1
+
+print(websites)
